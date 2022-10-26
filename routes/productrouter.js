8540efb3e9a37ffc7feb08c7d77b91a85abf5f42
elpatryk <patrykzmurko@yahoo.com>
2022-10-26 10:38:44 +0200

    pagination done

--- routes/productrouter.js
+++ routes/productrouter.js
@@ -5,18 +5,19 @@ const { Router } = express;
 
 const router = new Router();
 router.get("/", async (req, res, next) => {
+  const limit = req.query.limit || 5;
+  const offset = req.query.offset || 0;
   try {
-    const allProducts = await Products.findAll();
-    res.send(allProducts);
-  } catch (e) {
-    console.log(e.message);
-    next(e);
+    const result = await Products.findAndCountAll({ limit, offset });
+    res.send({ products: result.rows, total: result.count });
+  } catch (error) {
+    next(error);
   }
 });
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const getById = await Products.findByPk({ id });
+    const getById = await Products.findByPk(id);
     res.send(getById);
   } catch (e) {
     console.log(e.message);
